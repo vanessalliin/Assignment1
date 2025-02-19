4914655eb41f1b6429f8aa1d8acc9ea5a798dd66
vanessalliin <123032124+vanessalliin@users.noreply.github.com>
2025-02-19 17:52:53 +0000

    final

--- index.js
+++ index.js
@@ -27,8 +27,78 @@ class Chat extends HTMLElement {
   }
 
   connectedCallback() {
+    const suffix = (Math.random() * 100).toFixed().toString();
     this.innerHTML = `
-      <div class="chat-box">
+      <style>
+        .chat${suffix} ul { list-style: none; } 
+        /* chatbox */
+        .chat${suffix} {
+          max-width: 400px;
+          min-height: 400px;
+          background-color: #fff;
+          padding-right: 15px;
+          padding-left: 15px;
+          border-radius: 1rem;
+        }
+
+        /* messages */
+        .chat${suffix} .messages {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          height: 500px;
+        }
+
+        .chat${suffix} .message-list {
+          overflow-y: auto;
+          max-height: 500px;
+        }
+
+        .chat${suffix} .message-item {
+          padding: 20px;
+          border-radius: 0.75rem;
+          margin: 20px 0;
+        }
+
+        .chat${suffix} .message-item:last-child {
+          margin-bottom: 0;
+        }
+
+        .chat${suffix} .item-primary {
+          background-color: #f6f7f8;
+          color: #3c3c3e;
+          margin-right: 2em;
+        }
+
+        .chat${suffix} .item-secondary {
+          background-color:rgb(223, 63, 23);
+          color: #fff;
+          margin-left: 2em;
+        }
+
+        /* messages input */
+        .chat${suffix} .message-input {
+          display: flex;
+          padding: 20px 0;
+        }
+
+        .chat${suffix} .message-input input {
+          width: 100%;
+          padding: 10px;
+          border-radius: 2rem;
+          border: 1px solid #a5a5a5;
+        }
+
+        .chat${suffix} .message-input button {
+          padding: 10px;
+          margin-left: 10px;
+          border-radius: 5px;
+          border: none;
+          cursor: pointer;
+        }
+      </style>
+
+      <div class="chat${suffix}">
         <div class="messages">
           <ul class="message-list"></ul>
           <form class="message-input">
