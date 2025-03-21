585820183117fd893e6233b175cd21e8019eecc4
vanessalliin <123032124+vanessalliin@users.noreply.github.com>
2025-03-21 11:47:40 -0400

    Update fab.js

--- fab.js
+++ fab.js
@@ -50,7 +50,7 @@ document.querySelector("body").insertAdjacentHTML("beforeend", `
         right: 1em;
     }
     </style>
-    <button id="fab${suffix}">Tell me</button>
+    <button id="fab${suffix}">Open Chat Bot</button>
     `);
 
 document.querySelector(`#fab${suffix}`).addEventListener("click", evt=>{
